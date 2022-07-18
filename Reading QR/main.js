1a98d53dfc69f62c134c4403e92fa843820f5957
Soumyanil Das <dassic@outlook.com>
2022-07-18 21:41:21 +0530

    GET Request Sent

--- Reading QR/main.js
+++ Reading QR/main.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+axios.get('https://cors.io/?https://hackattic.com/challenges/reading_qr/problem?access_token=9a818cf05c282f8c', {
+    mode: 'cors',
+    method: 'GET',
+    headers: { 'Access-Control-Allow-Origin': true },
+})
+    .then(response => {
+        console.log(response);
+    })
+    .catch(er => {
+        console.log(er);
+    });
