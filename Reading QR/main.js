a9bf90db95c98c0f182eea459259f39c986bc3a4
Soumyanil Das <dassic@outlook.com>
2022-07-24 13:55:15 +0530

    Re-Added Fetch

--- Reading QR/main.js
+++ Reading QR/main.js
@@ -1,27 +1,27 @@
 console.log("hello");
 
-axios.get('https://cors-anywhere.herokuapp.com/https://hackattic.com/challenges/reading_qr/problem?access_token=9a818cf05c282f8c', {
-    method: 'GET',
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-    },
-})
-    .then(response => {
-        console.log(response.data.image_url)
-        // response.data.image_url.blob();
-    })
-    // .then(blob => {
-    //     console.log(blob);
-    // })
-    .catch(er => {
-        console.log(er);
-    });
-
-
-// fetch('https://cors-anywhere.herokuapp.com/https://hackattic.com/challenges/reading_qr/problem?access_token=9a818cf05c282f8c',)
+// axios.get('https://cors-anywhere.herokuapp.com/https://hackattic.com/challenges/reading_qr/problem?access_token=9a818cf05c282f8c', {
+//     method: 'GET',
+//     headers: {
+//         'Access-Control-Allow-Origin': '*',
+//     },
+// })
 //     .then(response => {
-//         console.log(response);
+//         console.log(response.data.image_url)
+//         // response.data.image_url.blob();
 //     })
-//     .catch(err => {
-//         console.log(err);
+//     // .then(blob => {
+//     //     console.log(blob);
+//     // })
+//     .catch(er => {
+//         console.log(er);
 //     });
+
+
+fetch('https://hackattic.com/challenges/reading_qr/problem?access_token=9a818cf05c282f8c',)
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.log(err);
+    });
