71820d8c032b9b966819cc88b73591c8cfd61d18
Soumyanil Das <dassic@outlook.com>
2022-07-25 14:10:21 +0530

    reviewed few changes

--- Reading QR/index.js
+++ Reading QR/index.js
@@ -1,4 +1,10 @@
-fetch('https://hackattic.com/challenges/reading_qr/problem?access_token=9a818cf05c282f8c')
+fetch('https://hackattic.com/challenges/reading_qr/problem?access_token=9a818cf05c282f8c', {
+    method: 'GET',
+
+})
+    .then(res => {
+        console.log(res.blob);
+    })
     .then(res => {
         console.log(res);
     })
